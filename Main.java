47eb24c75722f8a127a058b39e56b88ad90ef2ab
Karthikey Sharma <karthikeysharma22@gmail.com>
2023-10-05 03:10:39 +0530

    sum added

--- Main.java
+++ Main.java
@@ -8,5 +8,11 @@ public class Main{
                     System.out.println("Added one more file");
                     Student student = new Student();
                     student.sayHello();
+
+                    int a = 22;
+                    int b = 33;
+                    int c = a + b;
+                    System.out.println("The sum of a and b is :-" + c);
+                    System.out.println("This is the ans");
           }
 }
