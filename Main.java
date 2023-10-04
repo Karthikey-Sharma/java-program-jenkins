5a7d1885735a1a8f3c77cdba26522cd455e83c1f
Karthikey Sharma <karthikeysharma22@gmail.com>
2023-10-05 03:15:29 +0530

    sub added

--- Main.java
+++ Main.java
@@ -14,5 +14,9 @@ public class Main{
                     int c = a + b;
                     System.out.println("The sum of a and b is :-" + c);
                     System.out.println("This is the ans");
+
+                    System.out.println();
+                    int d = a- c;
+                    System.out.println("The subtraction of a and b is " + c);
           }
 }
